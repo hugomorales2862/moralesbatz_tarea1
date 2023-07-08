ecaf50ab65047bd6d7e8fbf8c5e1b8462f46de2d
Hugo Morales <moralesbatzh@gmail.com>
2023-07-07 20:43:50 -0600

    creamos la variable con un dato de tipo numero

--- moralesbatz_tarea1.js
+++ moralesbatz_tarea1.js
@@ -10,3 +10,10 @@ console.log(posicion);
 // dentro de la cadena texto y devuelve la posición donde se encuentra el primer carácter "," en la cadena original
 
 
+// se crea la variable de tipo numero 
+var numero = 42;
+// Método toFixed: redondea y devuelve una cadena con una cantidad específica de decimales
+console.log(numero.toFixed(2)); 
+// Método toString: convierte el número en una cadena
+console.log(numero.toString()); 
+
