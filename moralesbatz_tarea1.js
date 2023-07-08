43d3eb984e4fd93acf081f587cbc4887ed332b07
Hugo Morales <moralesbatzh@gmail.com>
2023-07-07 20:41:59 -0600

    variable de tipo string

---
+++ moralesbatz_tarea1.js
@@ -0,0 +1,12 @@
+
+//se crea la variable de tipo string
+var texto = "CODIGOS, DE PROGRAMACION";
+console.log(texto);
+console.log(texto.length);
+var posicion = texto.indexOf(",");
+console.log(posicion);
+//se utiliza el metodo length para saber la longitud del string
+//se utiliza el metodo  indexOf  busca el carácter ","
+// dentro de la cadena texto y devuelve la posición donde se encuentra el primer carácter "," en la cadena original
+
+
